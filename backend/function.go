612a9e04db13df8d6d5a60f4cbccbfa11a41198b
imranparuk <imranparuk@live.com>
2023-07-26 22:05:52 +0200

    updates

---
+++ backend/function.go
@@ -0,0 +1,12 @@
+package function
+
+import (
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/exolutionza/propfix-backend-go/internal/router"
+
+)
+
+func init() {
+	functions.HTTP("Router", router.Router)
+}
+
