f0d2d8c8aead0a11c1be50da0bfb438a41b0e559
imranparuk <imranparuk@live.com>
2023-07-31 01:02:18 +0200

    updates

--- backend/internal/handlers/board.go
+++ backend/internal/handlers/board.go
@@ -29,21 +29,36 @@ type CommentJson struct {
 	AvatarURL string    `json:"avatarUrl"`
 	Text      string    `json:"text"`
 }
+
+type AssigneeJson struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	AvatarURL string `json:"avatarUrl"`
+}
+
+type ReporterJson struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	AvatarURL string `json:"avatarUrl"`
+}
+
 type JobJson struct {
-	ID             string        `json:"id"`
-	Name           string        `json:"name"`
-	DueDate        time.Time     `json:"dueDate"`
-	Priority       string        `json:"priority"`
-	Description    string        `json:"description"`
-	ReporterID     string        `json:"reporterId"`
-	AssigneeIDs    []string      `json:"assigneeIds"`
-	UnitIdentifier string        `json:"unitIdentifier"`
-	BuildingID     string        `json:"buildingId"`
-	Labels         []string      `json:"labels"`
-	AttachmentURLs []string      `json:"attachmentUrls"`
-	Cost           float64       `json:"cost"`
-	CreatedAt      time.Time     `json:"createdAt"`
-	Comments       []CommentJson `json:"comments"`
+	ID             string         `json:"id"`
+	Name           string         `json:"name"`
+	DueDate        time.Time      `json:"dueDate"`
+	Priority       string         `json:"priority"`
+	Description    string         `json:"description"`
+	ReporterID     string         `json:"reporterId"`
+	AssigneeIDs    []string       `json:"assigneeIds"`
+	UnitIdentifier string         `json:"unitIdentifier"`
+	BuildingID     string         `json:"buildingId"`
+	Labels         []string       `json:"labels"`
+	AttachmentURLs []string       `json:"attachmentUrls"`
+	Cost           float64        `json:"cost"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	Comments       []CommentJson  `json:"comments"`
+	Assignees      []AssigneeJson `json:"assignees"`
+	Reporter       ReporterJson   `json:"reporter"`
 }
 
 func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
@@ -105,36 +120,17 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Failed to read job data", http.StatusInternalServerError)
 			return
 		}
-
 		// Assuming the job ID is present in the task, use it as the key
 		// Create a new JobJson object with the comments field
-		jobWithComments := JobJson{
-			ID:             job.ID,
-			Name:           job.Name,
-			DueDate:        job.DueDate,
-			Priority:       job.Priority,
-			Description:    job.Description,
-			ReporterID:     job.ReporterID,
-			AssigneeIDs:    job.AssigneeIDs,
-			UnitIdentifier: job.UnitIdentifier,
-			BuildingID:     job.BuildingID,
-			Labels:         job.Labels,
-			AttachmentURLs: job.AttachmentURLs,
-			Cost:           job.Cost,
-			CreatedAt:      job.CreatedAt,
-		}
-
-		// Fetch comments data for the given job ID and store it in the job data
-		comments, err := getCommentsForJobID(ctx, h.client, job.ID)
+		jobWithData, err := getData(ctx, h.client, job)
 		if err != nil {
 			fmt.Println(err)
+			http.Error(w, "Failed to fetch job data with comments", http.StatusInternalServerError)
+			return
 		}
 
-		// Update the jobWithComments with comments data
-		jobWithComments.Comments = comments
-
 		// Add the jobWithComments to jobsData
-		jobsData[job.ID] = jobWithComments
+		jobsData[job.ID] = jobWithData
 	}
 
 	// ... (existing code)
@@ -152,8 +148,37 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+// Helper function to fetch data for a given job ID
+func getData(ctx context.Context, client *bigquery.Client, jobData JobJson) (JobJson, error) {
+	// Fetch comments data for the given job ID
+	comments, err := getComments(ctx, client, jobData.ID)
+	if err != nil {
+		return jobData, err
+	}
+
+	// Fetch assignees data for the given assignee IDs
+	assignees, err := getAssignees(ctx, client, jobData.AssigneeIDs)
+	if err != nil {
+		return jobData, err
+	}
+	fmt.Println(assignees)
+	// Fetch reporter data for the given reporter ID
+	reporter, err := getReporter(ctx, client, jobData.ReporterID)
+	if err != nil {
+		return jobData, err
+	}
+	fmt.Println(reporter)
+
+	// Populate the jobData with the fetched data
+	jobData.Comments = comments
+	jobData.Assignees = assignees
+	jobData.Reporter = reporter
+
+	return jobData, nil
+}
+
 // Helper function to fetch comments data for a given job ID
-func getCommentsForJobID(ctx context.Context, client *bigquery.Client, jobID string) ([]CommentJson, error) {
+func getComments(ctx context.Context, client *bigquery.Client, jobID string) ([]CommentJson, error) {
 	query := fmt.Sprintf(`
 		SELECT
 			c.id, c.text, c.createdat, c.jobid, c.memberId, m.name, m.email, m.role, m.avatarurl
@@ -187,4 +212,75 @@ func getCommentsForJobID(ctx context.Context, client *bigquery.Client, jobID str
 	}
 
 	return comments, nil
+}
+
+// Helper function to fetch assignees data for a given list of assignee IDs
+func getAssignees(ctx context.Context, client *bigquery.Client, assigneeIDs []string) ([]AssigneeJson, error) {
+	// If there are no assignee IDs, return an empty slice
+	if len(assigneeIDs) == 0 {
+		return nil, nil
+	}
+
+	// Construct the IN clause for the assignee IDs
+	assigneeIDStr := "'" + assigneeIDs[0] + "'"
+	for i := 1; i < len(assigneeIDs); i++ {
+		assigneeIDStr += ",'" + assigneeIDs[i] + "'"
+	}
+
+	query := fmt.Sprintf(`
+		SELECT
+			id, name, avatarurl
+		FROM
+			propfix.main.members
+		WHERE
+			id IN (%s)
+	`, assigneeIDStr)
+
+	assigneeQuery := client.Query(query)
+	assigneeIterator, err := assigneeQuery.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var assignees []AssigneeJson
+	for {
+		var assignee AssigneeJson
+		err := assigneeIterator.Next(&assignee)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		assignees = append(assignees, assignee)
+	}
+
+	return assignees, nil
+}
+
+// Helper function to fetch reporter data for a given reporter ID
+func getReporter(ctx context.Context, client *bigquery.Client, reporterID string) (ReporterJson, error) {
+	query := fmt.Sprintf(`
+		SELECT
+			id, name, avatarurl
+		FROM
+			propfix.main.members
+		WHERE
+			id = '%s'
+	`, reporterID)
+	fmt.Println(reporterID)
+	reporterQuery := client.Query(query)
+	reporterIterator, err := reporterQuery.Read(ctx)
+	if err != nil {
+		return ReporterJson{}, err
+	}
+
+	var reporter ReporterJson
+	err = reporterIterator.Next(&reporter)
+
+	if err != nil {
+		return ReporterJson{}, err
+	}
+
+	return reporter, nil
 }
