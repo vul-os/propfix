f5c96456a118abeafc62fd2bb8a6b24da8bb462c
spa <spa@example.com>
2023-08-07 16:59:27 +0200

    broken commit

--- backend/internal/board/handler.go
+++ backend/internal/board/handler.go
@@ -3,6 +3,7 @@ package board
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"sort"
 
@@ -13,7 +14,8 @@ import (
 	"github.com/exolutionza/propfix-backend-go/internal/columns"
 	"github.com/exolutionza/propfix-backend-go/internal/jobs"
 	pUser "github.com/exolutionza/propfix-backend-go/internal/user"
-
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"google.golang.org/api/iterator"
 )
 
@@ -33,13 +35,19 @@ func NewBoardHandler(client *bigquery.Client, authClient *fAuth.Client, authz *a
 
 func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	user, ok := r.Context().Value("user").(pUser.User) // Replace "user.User" with the actual user type from your authentication mechanism
+
+	// Retrieve the user from the request context
+	user, ok := r.Context().Value("user").(pUser.User)
 	if !ok {
 		http.Error(w, "Failed to get user details", http.StatusInternalServerError)
 		return
 	}
 
-	// Check if the user has the permission to update events
+	// Retrieve the boardId from the route variables using Gorilla Mux
+	vars := mux.Vars(r)
+	boardId := vars["boardId"]
+
+	// Check if the user has the permission to update events for the specified board
 	if hasPermission, err := h.authz.CheckPermission(user.ID, "board", "get"); err != nil {
 		http.Error(w, "Failed to check permission", http.StatusInternalServerError)
 		return
@@ -48,8 +56,8 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Query the columns table
-	columnsQuery := h.client.Query("SELECT id, name, jobids FROM propfix.main.columns")
+	// Query the columns table to fetch columns associated with the boardId
+	columnsQuery := h.client.Query(fmt.Sprintf("SELECT id, name, jobids FROM propfix.main.columns WHERE boardId = '%s'", boardId))
 	columnsIterator, err := columnsQuery.Read(ctx)
 	if err != nil {
 		http.Error(w, "Failed to fetch columns", http.StatusInternalServerError)
@@ -71,9 +79,10 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 
 		// Construct the Column object
 		columnData := columns.Column{
-			ID:     col.ID,
-			Name:   col.Name,
-			JobIDs: col.JobIDs,
+			ID:      col.ID,
+			Name:    col.Name,
+			JobIDs:  col.JobIDs,
+			BoardID: col.BoardID,
 		}
 		columnsData[col.ID] = columnData
 		orderedColumns = append(orderedColumns, col.ID)
@@ -82,9 +91,9 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 	// Sort the ordered columns to ensure consistent order
 	sort.Strings(orderedColumns)
 
-	// Fetch the jobs
-	query := h.client.Query("SELECT * FROM propfix.main.jobs")
-	jobsIterator, err := query.Read(ctx)
+	// Query the jobs table to fetch jobs associated with the boardId
+	jobsQuery := h.client.Query(fmt.Sprintf("SELECT * FROM propfix.main.jobs WHERE boardId = '%s'", boardId))
+	jobsIterator, err := jobsQuery.Read(ctx)
 	if err != nil {
 		http.Error(w, "Failed to fetch jobs", http.StatusInternalServerError)
 		return
@@ -156,11 +165,127 @@ func (h *BoardHandler) GetBoard(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-// Helper function to get keys from a map[string]bool and return them as a slice.
 func getKeysFromMap(m map[string]bool) []string {
 	keys := make([]string, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
 	}
 	return keys
+}
+
+func (h *BoardHandler) CreateBoard(w http.ResponseWriter, r *http.Request) {
+	// Get the user from the request context
+	user, ok := r.Context().Value("user").(pUser.User)
+	if !ok {
+		http.Error(w, "Failed to get user details", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if the user has the permission to create boards
+	if hasPermission, err := h.authz.CheckPermission(user.ID, "board", "create"); err != nil {
+		http.Error(w, "Failed to check permission", http.StatusInternalServerError)
+		return
+	} else if !hasPermission {
+		http.Error(w, "You do not have permission to create boards", http.StatusForbidden)
+		return
+	}
+
+	var board Board
+	err := json.NewDecoder(r.Body).Decode(&board)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	board.ID = uuid.New().String()
+
+	ctx := context.Background()
+	inserter := h.client.Dataset("main").Table("Boards").Inserter()
+	err = inserter.Put(ctx, &board)
+	if err != nil {
+		http.Error(w, "Failed to create board", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *BoardHandler) UpdateBoard(w http.ResponseWriter, r *http.Request) {
+	// Get the user from the request context
+	user, ok := r.Context().Value("user").(pUser.User)
+	if !ok {
+		http.Error(w, "Failed to get user details", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if the user has the permission to update boards
+	if hasPermission, err := h.authz.CheckPermission(user.ID, "board", "update"); err != nil {
+		http.Error(w, "Failed to check permission", http.StatusInternalServerError)
+		return
+	} else if !hasPermission {
+		http.Error(w, "You do not have permission to update boards", http.StatusForbidden)
+		return
+	}
+
+	var board Board
+	err := json.NewDecoder(r.Body).Decode(&board)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.Background()
+	q := h.client.Query(fmt.Sprintf(`
+		UPDATE main.Boards
+		SET name = @name
+		WHERE id = @boardID
+	`))
+	q.Parameters = []bigquery.QueryParameter{
+		{Name: "name", Value: board.Name},
+		{Name: "boardID", Value: board.ID},
+	}
+
+	_, err = q.Run(ctx)
+	if err != nil {
+		http.Error(w, "Failed to update board", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *BoardHandler) DeleteBoard(w http.ResponseWriter, r *http.Request) {
+	// Get the user from the request context
+	user, ok := r.Context().Value("user").(pUser.User)
+	if !ok {
+		http.Error(w, "Failed to get user details", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if the user has the permission to delete boards
+	if hasPermission, err := h.authz.CheckPermission(user.ID, "board", "delete"); err != nil {
+		http.Error(w, "Failed to check permission", http.StatusInternalServerError)
+		return
+	} else if !hasPermission {
+		http.Error(w, "You do not have permission to delete boards", http.StatusForbidden)
+		return
+	}
+
+	vars := mux.Vars(r)
+	boardID := vars["boardId"]
+
+	ctx := context.Background()
+	q := h.client.Query(fmt.Sprintf(`
+		DELETE FROM main.Boards
+		WHERE id = @boardID
+	`))
+	q.Parameters = []bigquery.QueryParameter{{Name: "boardID", Value: boardID}}
+
+	_, err := q.Run(ctx)
+	if err != nil {
+		http.Error(w, "Failed to delete board", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
---
+++ backend/internal/organisations/handler.go
@@ -0,0 +1,148 @@
+package organizations
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/gorilla/mux"
+	"google.golang.org/api/iterator"
+)
+
+type OrganizationHandler struct {
+	client *bigquery.Client
+}
+
+type Organization struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Members []Member `json:"members"`
+}
+
+type Member struct {
+	UserID string `json:"userId,omitempty"`
+	Email  string `json:"email,omitempty"`
+}
+
+func NewOrganizationHandler(client *bigquery.Client) *OrganizationHandler {
+	return &OrganizationHandler{
+		client: client,
+	}
+}
+
+func (h *OrganizationHandler) CreateOrganization(w http.ResponseWriter, r *http.Request) {
+	var org Organization
+	err := json.NewDecoder(r.Body).Decode(&org)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Check if organization already exists
+	ctx := context.Background()
+	query := h.client.Query(fmt.Sprintf(`
+		SELECT id FROM propfix.Organizations WHERE id = @orgID
+	`))
+	query.Parameters = []bigquery.QueryParameter{{Name: "orgID", Value: org.ID}}
+	it, err := query.Read(ctx)
+	if err != nil {
+		http.Error(w, "Failed to check organization existence", http.StatusInternalServerError)
+		return
+	}
+	var existingOrg Organization
+	err = it.Next(&existingOrg)
+	if err != iterator.Done {
+		http.Error(w, "Organization already exists", http.StatusConflict)
+		return
+	}
+
+	// Insert the organization into the BigQuery table
+	inserter := h.client.Dataset("propfix").Table("Organizations").Inserter()
+	err = inserter.Put(ctx, &org)
+	if err != nil {
+		http.Error(w, "Failed to create organization", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *OrganizationHandler) GetOrganization(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orgID := vars["id"]
+
+	ctx := context.Background()
+	query := h.client.Query(fmt.Sprintf(`
+		SELECT id, name, members
+		FROM propfix.Organizations
+		WHERE id = @orgID
+	`))
+	query.Parameters = []bigquery.QueryParameter{{Name: "orgID", Value: orgID}}
+	it, err := query.Read(ctx)
+	if err != nil {
+		http.Error(w, "Organization not found", http.StatusNotFound)
+		return
+	}
+	var org Organization
+	err = it.Next(&org)
+	if err != nil {
+		http.Error(w, "Organization not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(org)
+}
+
+func (h *OrganizationHandler) UpdateOrganization(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orgID := vars["id"]
+
+	var org Organization
+	err := json.NewDecoder(r.Body).Decode(&org)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.Background()
+	query := h.client.Query(fmt.Sprintf(`
+		UPDATE propfix.Organizations
+		SET name = @name
+		WHERE id = @orgID
+	`))
+	query.Parameters = []bigquery.QueryParameter{
+		{Name: "name", Value: org.Name},
+		{Name: "orgID", Value: orgID},
+	}
+
+	_, err = query.Run(ctx)
+	if err != nil {
+		http.Error(w, "Failed to update organization", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *OrganizationHandler) DeleteOrganization(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orgID := vars["id"]
+
+	ctx := context.Background()
+	query := h.client.Query(fmt.Sprintf(`
+		DELETE FROM propfix.Organizations
+		WHERE id = @orgID
+	`))
+	query.Parameters = []bigquery.QueryParameter{{Name: "orgID", Value: orgID}}
+
+	_, err := query.Run(ctx)
+	if err != nil {
+		http.Error(w, "Failed to delete organization", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
