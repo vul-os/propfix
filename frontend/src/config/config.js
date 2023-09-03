218b3cf1299a1842dcda7696336c2ab428878513
exolutiontech <exolutionza@gmail.com>
2023-09-03 23:01:32 +0200

    updates

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,27 +1,27 @@
 // config.js
 // LOOK HERE! Never put a trailing '/' eg: https://abc.com/ <- dont do that
 function getConfig() {
-    const defaultConfig = {
-      apiUrl: 'https://propfix-backend-go-mm4ahu6lbq-uc.a.run.app/api/authenticated/',
-    };
+  const defaultConfig = {
+    apiUrl: 'https://propfix-backend-go-mm4ahu6lbq-uc.a.run.app',
+  };
+
+  const location = window.location.href;
   
-    const location = window.location.href;
-    
-    switch (true) {
-      case location.includes('propfix'):
-        return defaultConfig;
-      case location.includes('localhost'):
-        return {
-          apiUrl: 'http://localhost:8080',
-        };
-      case location.includes('gitpod'):
-        return defaultConfig;
-      default:
-        return defaultConfig;
-    }
+  switch (true) {
+    case location.includes('propfix'):
+      return defaultConfig;
+    case location.includes('localhost'):
+      return {
+        apiUrl: 'http://localhost:8080',
+      };
+    case location.includes('gitpod'):
+      return defaultConfig;
+    default:
+      return defaultConfig;
   }
-  
+}
 
-  const config = getConfig();
 
-  export default config;  
+const config = getConfig();
+
+export default config;  
