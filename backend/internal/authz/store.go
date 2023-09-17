113a2792eec794e7b95489edcabb7a8f94b88f7d
exolutiontech <exolutionza@gmail.com>
2023-09-17 22:42:18 +0200

    updates

--- backend/internal/authz/store.go
+++ backend/internal/authz/store.go
@@ -182,7 +182,6 @@ func (s *Authz) CheckPermission(r *http.Request, resource string, permission str
 		LIMIT 1
 	)
 	`
-	fmt.Println(sqlQuery, user.ID, roleIDs, resource, permission)
 	var hasPermission bool
 	err = s.dbpool.QueryRow(ctx, sqlQuery, user.ID, roleIDs, resource, permission).Scan(&hasPermission)
 	if err != nil {
