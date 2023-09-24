ce7274a95b9abc5725c846c1b54d8af82a9c4cb3
desanza <desannaidoo48@gmail.com>
2023-09-24 20:28:07 +0200

    update_filters

--- frontend/src/pages/jobs/data-grid/data-grid.js
+++ frontend/src/pages/jobs/data-grid/data-grid.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import FilterListIcon from '@mui/icons-material/FilterList'; // Import the Material-UI filter icon
 import moment from 'moment';
-import IntroJs from 'intro.js';
 import { styled } from '@mui/material/styles';
 import { DataGrid } from '@mui/x-data-grid';
 import Avatar from '@mui/material/Avatar';
@@ -15,39 +13,51 @@ import AddIcon from '@mui/icons-material/Add';
 import Chip from '@mui/material/Chip';
 import EventIcon from '@mui/icons-material/Event';
 import HomeIcon from '@mui/icons-material/Home'; 
-import Button from '@mui/material/Button'; 
+import Button from '@mui/material/Button'; // Import the Button component from Material-UI
 import { Icon } from '@iconify/react';
+import Drawer from '@mui/material/Drawer';
+import TextField from '@mui/material/TextField'; // Import TextField for form control
+import FormControl from '@mui/material/FormControl';
 import Iconify from '../../../components/iconify';
 import PopOver from '../pop-over';
-import { useBoardContext } from '../../../contexts/board';
+import { useBoardContext } from '../../../contexts/board'; // Import the BoardProvider context
 import CreateJobDialog from '../../job-wizzard/dialog';
 import { exportToCSV, exportToExcel } from './utils';
-import 'intro.js/introjs.css';
+
+
 
 const StyledDataGrid = styled(DataGrid)(() => ({
   '& .super-app-theme--Open': {
-    backgroundColor: "rgba(255, 0, 0, 0.2)",
+    backgroundColor: "rgba(255, 0, 0, 0.2)", // slight red
     '&:hover': {
-      backgroundColor: "rgba(255, 0, 0, 0.3)",
+      backgroundColor: "rgba(255, 0, 0, 0.3)", // slight red
     }
   },
   '& .super-app-theme--Closed': {
-    backgroundColor: "",
+    backgroundColor: "", // slight red
   },
 }));
 
 function JobDataGrid() {
-  const navigate = useNavigate()
   const { board, jobs, boardLoading } = useBoardContext(); // Use the BoardProvider context
   const [open, setOpen] = useState(false);
   const [selectedRow, setSelectedRow] = useState(null);
 
+const [filterOpen, setFilterOpen] = useState(false); // Add filter state
+const [sidebarOpen, setSidebarOpen] = useState(false);
+
+
+const toggleSidebar = () => {
+  setSidebarOpen(!sidebarOpen);
+};
+
+  
    const onClose = () => {
     setOpen(false);
   }
 
   const highlightRowStyle = {
-    backgroundColor: "rgba(255, 0, 0, 0.01)",
+    backgroundColor: "rgba(255, 0, 0, 0.01)", // slight red
   };
 
   const avatarRenderer = (params) => {
@@ -63,33 +73,37 @@ function JobDataGrid() {
       </Stack>
     );
   };
-
+  
+  
   const renderLabel = (params) => {
     if (!(params && params.value && params.value.length)) {
-      return null;
+      return null; // Return null if there are no labels to render
     }
-
+  
+    // Calculate the label lengths and create an array of objects
     const labeledChips = params.value.map((labelId) => ({
       label: board?.labels[labelId] ? board.labels[labelId].name : "",
       length: board?.labels[labelId] ? board.labels[labelId].name.length : 0,
     }));
-
+  
+    // Sort the labeledChips array based on label length in ascending order
     labeledChips.sort((a, b) => a.length - b.length);
 
+  
     return (
       <Stack direction="row">
         <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
           {labeledChips.map((labeledChip, index) => (
             <Chip
-              key={index}
+              key={index} // Use index as the key since labels may have the same length
               style={{
                 backgroundColor: board?.labels[params.value[index]]
                   ? board.labels[params.value[index]].color
                   : "red",
-                color: 'white',
-                marginRight: '8px',
-                marginBottom: index === labeledChips.length - 1 ? '4px' : '2px',
-                marginTop: index === 0 ? '4px' : '0px',
+                color: 'white', // Set text color to white
+                marginRight: '8px', // Add margin between chips
+                marginBottom: index === labeledChips.length - 1 ? '4px' : '2px', // Add margin at the bottom for the last chip in the row
+                marginTop: index === 0 ? '4px' : '0px', // Add margin at the top for the first chip in the row
               }}
               label={labeledChip.label}
               size="small"
@@ -100,7 +114,9 @@ function JobDataGrid() {
       </Stack>
     );
   };
-
+  
+  
+  
   const renderDate = (params) => {
     const formattedDate = formatDate(params.value);
     return (
@@ -115,30 +131,28 @@ function JobDataGrid() {
     const building = params.value && board?.buildings[params.value]?.buildingName;
     return (
       <Stack direction="row" alignItems="center">
-        { building && 
-          <HomeIcon sx={{ marginRight: 2 }} /> 
-        }
-        <span>{building}</span> 
+        <HomeIcon sx={{ marginRight: 2 }} /> {/* Home icon with 1rem (10px) right margin */}
+        <span>{building}</span> {/* Building ID value */}
       </Stack>
     );
   };
-
+  
   const renderPriority = (params) => {
     let { value: priority } = params;
     priority = priority.toLowerCase();
-
+  
     const getIcon = () => {
       if (priority === 'low') return 'solar:double-alt-arrow-down-bold-duotone';
       if (priority === 'medium') return 'solar:double-alt-arrow-right-bold-duotone';
       return 'solar:double-alt-arrow-up-bold-duotone';
     };
-
+  
     const getIconColor = () => {
       if (priority === 'low') return 'info.main';
       if (priority === 'medium') return 'warning.main';
       return 'error.main';
     };
-
+  
     return (
       <Stack direction="row" alignItems="center">
         <Iconify
@@ -165,9 +179,9 @@ function JobDataGrid() {
       field: 'unitIdentifier',
       headerName: 'Unit Number',
       width: 200,
-      valueGetter: (params) => params.row.unitIdentifier?.toUpperCase(),
+      valueGetter: (params) => params.row.unitIdentifier?.toUpperCase(), // Convert cell content to uppercase
       renderCell: (params) => (
-        <strong>{params.value}</strong>
+        <strong>{params.value}</strong> // Wrap cell content in a <strong> element for bold text
       ),
     },
     {
@@ -192,6 +206,12 @@ function JobDataGrid() {
       renderCell: renderPriority,
     },
     { field: 'description', headerName: 'Description', width: 200 },
+    // {
+    //   field: 'reporter',
+    //   headerName: 'Reporter',
+    //   width: 200,
+    //   renderCell: avatarRenderer,
+    // },
     {
       field: 'assigneeIds',
       headerName: 'Assignees',
@@ -199,7 +219,8 @@ function JobDataGrid() {
       renderCell: avatarRenderer,
     },
 
-    {
+     // Add the Hour column after the Assignees column
+     {
       field: 'hours',
       headerName: 'Hours',
       width: 100,
@@ -209,35 +230,18 @@ function JobDataGrid() {
 
     { field: 'cost', headerName: 'Cost', type: 'number', width: 70 , headerAlign: '-5px', align:'-4px', },
     { field: 'closedAt', headerName: 'Closed At', width: 160, renderCell: renderDate },
+
   ];
 
   const handleRowClick = (params) => {
     setSelectedRow(params.row);
   };
 
-  useEffect(() => {
-    const intro = IntroJs();
-
-    const introCompleted = localStorage.getItem('introCompleted');
-    if (1) {
-      intro.setOptions({
-        steps: [
-          {
-            element: document.querySelector('.intro-step-fab'),
-            intro: "Click this button to create a new job.",
-          },
-        ],
-        showStepNumbers: true,
-      });
-      intro.start();
-
-      localStorage.setItem('introCompleted', 'true');
-    }
-
-    return () => {
-      intro.exit();
+    // Function to handle filter button click
+    const handleFilterClick = () => {
+      toggleSidebar();
     };
-  }, []);
+    
 
   return (
     <Container maxWidth={false} sx={{ height: 1 }}>
@@ -249,45 +253,126 @@ function JobDataGrid() {
             sx={{ 
               backgroundColor: 'black;',
               color: 'white',
-              borderRadius: '50%',
-              minWidth: 0,
-              width: '40px',
-              height: '40px',
+              borderRadius: '50%', // Use 50% to make it round
+              minWidth: 0, // To prevent automatic width expansion
+              width: '40px', // Set a fixed width (adjust as needed)
+              height: '40px', // Set a fixed height (adjust as needed)
               WebkitBorderRadius: '30px',
               display: 'flex',
-              alignItems: 'center',
+              alignItems: 'center', // Align items vertically
+  
             }}
             size="small"
             onClick={() => exportToCSV(jobs, 'jobs')}
           >
             <Icon
-              icon="grommet-icons:document-csv"
-              style={{ fontSize:'20px', marginRight: '1.5px',}}
-            />
+            icon="grommet-icons:document-csv"
+            style={{ fontSize:'20px', marginRight: '1.5px',}} // Adjust the fontSize here
+  />
           </Button>
           <Button
             variant="contained"
             sx={{ 
               backgroundColor: 'black;',
               color: 'white',
-              borderRadius: '50%',
-              minWidth: 0,
-              width: '40px',
-              height: '40px',
+              borderRadius: '50%', // Use 50% to make it round
+              minWidth: 0, // To prevent automatic width expansion
+              width: '40px', // Set a fixed width (adjust as needed)
+              height: '40px', // Set a fixed height (adjust as needed)
               WebkitBorderRadius: '30px',
               display: 'flex',
-              alignItems: 'center',
+              alignItems: 'center', // Align items vertically
+  
             }}
             size="small"
             onClick={() => exportToExcel(jobs, 'jobs')}
           >
-            <Icon
-              icon="file-icons:microsoft-excel"
-              style={{ fontSize:'20px', marginRight: '1.5px', }}
-            />
+          <Icon
+            icon="file-icons:microsoft-excel"
+            style={{ fontSize:'20px', marginRight: '1.5px', }} // Adjust the fontSize here
+/>
           </Button>
         </Stack>
       </Typography>
+
+      <Button
+          variant="contained"
+          sx={{ 
+            backgroundColor: 'white',
+            color: 'black',
+            // borderRadius: '50%',
+            minWidth: 0,
+            width: '40px',
+            height: '40px',
+            marginRight: '160px',
+            marginLeft: 'auto', // Push the button to the right by setting marginLeft to auto
+            marginTop: '-80px', // Add a negative top margin to move the button up
+            marginBottom: '40px',
+            // WebkitBorderRadius: '30px',
+            display: 'flex',
+            alignItems: 'center',
+            '&:hover': {
+              backgroundColor: 'white', // Change background color on hover
+              color: 'black', // Change text color on hover
+            },
+          }}
+          size="small"
+          onClick={handleFilterClick} // Call the filter button click handler
+        >
+          <h3 style={{ margin: 0, fontSize: '17px', marginTop:'3px', marginRight: '10px'}}>Filters</h3> {/* Add this line for the "Filters" heading */}
+          <FilterListIcon style={{ fontSize: '20px', marginTop: '3px', }} />
+        </Button>
+
+         {/* Sidebar */}
+         <Drawer
+  anchor="right"
+  open={sidebarOpen}
+  onClose={toggleSidebar}
+>
+  <div style={{ width: '250px', padding: '16px' }}>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <h2 style={{ fontSize: '18px', margin: 0 }}>Filters</h2>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <button
+          onClick={() => {
+            // Handle reset filter logic here
+          }}
+          style={{
+            background: 'transparent',
+            border: 'none',
+            cursor: 'pointer',
+            marginRight: '8px',
+          }}
+        >
+          <Icon icon="carbon:reset" style={{ fontSize: '20px' }} />
+        </button>
+        <button
+          onClick={toggleSidebar}
+          style={{
+            background: 'transparent',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          <Icon icon="ph:x" style={{ fontSize: '20px' }} />
+        </button>
+      </div>
+    </div>
+
+    {/* Add the horizontal line below the "Filters" heading */}
+    <hr style={{ width: '100%', borderTop: '1px solid #DBDBDB', margin: '8px 0' }} />
+
+    {/* Add the "Created At" heading */}
+    <h3 style={{ fontSize: '16px', margin: '16px 0 8px 0' }}>Created At</h3>
+  </div>
+    {/* Add the data field */}
+</Drawer>
+
+
+
+
+      
+      
 
       {jobs && !boardLoading && (
         <StyledDataGrid
@@ -297,7 +382,7 @@ function JobDataGrid() {
           rowsPerPageOptions={[10]}
           checkboxSelection
           onRowClick={handleRowClick}
-          getRowHeight={() => 60}
+          getRowHeight={() => 60} // Set the desired row height (in pixels)
           getRowClassName={(params) => {
             const date = params.row.closedAt;
             if (date && date !== "0001-01-01T00:00:00Z" && moment(date).isValid()) {
@@ -305,7 +390,8 @@ function JobDataGrid() {
             }
             return `super-app-theme--Closed`;
           }}
-        />
+       />
+      
       )}
 
       {selectedRow && (
@@ -320,7 +406,6 @@ function JobDataGrid() {
         aria-label="add" 
         style={{ position: 'fixed', bottom: '75px', right: '16px' }} 
         onClick={() => setOpen(true)}
-        className="intro-step-fab"
       >
         <AddIcon />
       </Fab>
@@ -329,4 +414,4 @@ function JobDataGrid() {
   );
 }
 
-export default JobDataGrid;
+export default JobDataGrid; 
