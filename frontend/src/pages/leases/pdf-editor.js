a678e52ec6ffaf2bb5ae00a7af9406430a5264dd
imranparuk <imranparuk@live.com>
2023-10-18 23:36:58 +0200

    pdf

--- frontend/src/pages/leases/pdf-editor.js
+++ frontend/src/pages/leases/pdf-editor.js
@@ -1,76 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import TextFieldsIcon from '@mui/icons-material/TextFields';
-import ImageIcon from '@mui/icons-material/Image';
-import PanToolIcon from '@mui/icons-material/PanTool';
-import Box from '@mui/material/Box';
 import PDFViewer from './pdf-viewer';
 
 const PDFAreaEditor = () => {
-  const pdfUrl = "https://storage.googleapis.com/exo-public-bucket/LEASE-AGREEMENT-RESIDENTIAL-October-2019.pdf";
+  const pdfUrl = 'https://storage.googleapis.com/exo-public-bucket/LEASE-AGREEMENT-RESIDENTIAL-October-2019.pdf';
   const [pdfData, setPdfData] = useState(null);
-  const [elements, setElements] = useState([]);
 
   useEffect(() => {
     fetch(pdfUrl)
-    .then(response => response.arrayBuffer())
-    .then(buffer => {
-      const uint8Array = new Uint8Array(buffer);
-      setPdfData(uint8Array);
-    })
-    .catch(error => {
-      console.error("There was an error fetching the PDF", error);
-    });
+      .then((response) => response.arrayBuffer())
+      .then((buffer) => {
+        const uint8Array = new Uint8Array(buffer);
+        setPdfData(uint8Array);
+      })
+      .catch((error) => {
+        console.error('There was an error fetching the PDF', error);
+      });
   }, [pdfUrl]);
-
-  const addTextElement = () => {
-    setElements([...elements, {
-      type: 'text',
-      content: 'New Text',
-      position: { x: 50, y: 50 }
-    }]);
-  };
-
-  const addImageElement = () => {
-    const imageUrl = prompt('Enter the image URL:', '');
-    if (imageUrl) {
-      setElements([...elements, {
-        type: 'image',
-        content: imageUrl,
-        position: { x: 50, y: 100 }
-      }]);
-    }
-  };
-
-  const onSelectElement = () => {
-    // Logic for selecting elements
-    // This can be expanded upon based on specific requirements
-  };
 
   return (
     <div>
-      <Box display="flex" gap={1}>
-        <Tooltip title="Select">
-          <IconButton onClick={onSelectElement} aria-label="select">
-            <PanToolIcon />
-          </IconButton>
-        </Tooltip>
-
-        <Tooltip title="Add Text">
-          <IconButton onClick={addTextElement} aria-label="add text">
-            <TextFieldsIcon />
-          </IconButton>
-        </Tooltip>
-
-        <Tooltip title="Add Image">
-          <IconButton onClick={addImageElement} aria-label="add image">
-            <ImageIcon />
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <PDFViewer pdfData={pdfData} elements={elements} />
-      {/* Note: Your PDFViewer component would need to render these elements on top of the PDF */}
+      <h1>PDF Area Editor</h1>
+      <PDFViewer pdfData={pdfData} />
     </div>
   );
 };
--- frontend/src/pages/leases/pdf-viewer.js
+++ frontend/src/pages/leases/pdf-viewer.js
@@ -1,18 +1,91 @@
 import React, { useState, useEffect, useRef } from 'react';
 import * as PDFJS from 'pdfjs-dist/webpack';
+import { useDrag, useDrop, DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 PDFJS.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
 
-const PDFViewer = ({ pdfData, elements }) => {
+const ElementTypes = {
+  RECTANGLE: 'rectangle',
+  TEXT: 'text',
+};
+
+// DraggableText component
+const DraggableText = ({ text, position, size, index, updateElementPosition, onSelect, isSelected }) => {
+    const [, drag] = useDrag({
+      type: ElementTypes.TEXT,
+      item: { index },
+    });
+  
+    const handleMouseDown = (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      onSelect();
+    };
+  
+    const handleMouseMove = (e) => {
+      if (isSelected) {
+        if (e.ctrlKey) {
+          // Resize the element if Ctrl key is pressed
+          const newSize = {
+            width: size.width + e.movementX,
+            height: size.height + e.movementY,
+          };
+          updateElementPosition(index, position, newSize);
+        } else {
+          // Move the element if Ctrl key is not pressed
+          const newPosition = {
+            x: position.x + e.movementX,
+            y: position.y + e.movementY,
+          };
+          updateElementPosition(index, newPosition, size);
+        }
+      }
+    };
+  
+    const handleMouseUp = () => {
+      // Deselect the element when the mouse button is released
+      onSelect(null);
+    };
+  
+    return (
+      <div
+        role="button"
+        tabIndex={0}
+        onMouseDown={handleMouseDown}
+        onMouseMove={(e) => handleMouseMove(e)} // Pass the event object to handleMouseMove
+        onMouseUp={handleMouseUp}
+        style={{
+          position: 'absolute',
+          left: position.x,
+          top: position.y,
+          width: size?.width,
+          height: size?.height,
+          cursor: isSelected ? (1 ? 'nwse-resize' : 'move') : 'pointer',
+          border: isSelected ? '2px solid blue' : 'none',
+          outline: 'none',
+        }}
+      >
+        {text}
+      </div>
+    );
+  };
+  
+
+const PDFViewer = ({ pdfData }) => {
   const [pdfDoc, setPdfDoc] = useState(null);
   const [pageNum, setPageNum] = useState(1);
+  const [elements, setElements] = useState([]);
+  const [selectedElement, setSelectedElement] = useState(null);
+
   const [isRendering, setIsRendering] = useState(false);
+  const [addingText, setAddingText] = useState(false);
   const canvasRef = useRef(null);
 
-  const renderPage = num => {
+  const renderPage = (num) => {
     if (!pdfDoc || isRendering) return;
 
-    pdfDoc.getPage(num).then(page => {
+    pdfDoc.getPage(num).then((page) => {
       const viewport = page.getViewport({ scale: 1 });
       const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
@@ -29,19 +102,26 @@ const PDFViewer = ({ pdfData, elements }) => {
 
       renderTask.promise.then(() => {
         setIsRendering(false);
-        const img = new Image();
-
-        // Render elements on top of the PDF
-        elements.forEach(element => {
+        // Render elements on top of the PDF page
+        elements?.forEach((element) => {
           switch (element.type) {
             case 'text':
-              context.fillText(element.content, element.position.x, element.position.y);
+              context.font = `${element.fontSize}px Arial`; // Modify font as needed
+              context.fillStyle = 'black';
+              context.fillText(
+                element.content,
+                element.position.x,
+                element.position.y
+              );
               break;
-            case 'image':
-              img.src = element.content;
-              img.onload = () => {
-                context.drawImage(img, element.position.x, element.position.y);
-              };
+            case 'rectangle':
+              context.fillStyle = 'red';
+              context.fillRect(
+                element.position.x,
+                element.position.y,
+                element.width,
+                element.height
+              );
               break;
             default:
               break;
@@ -57,7 +137,7 @@ const PDFViewer = ({ pdfData, elements }) => {
     const clonedBuffer = pdfData.slice(0);
     const loadingTask = PDFJS.getDocument({ data: clonedBuffer });
 
-    loadingTask.promise.then(pdf => {
+    loadingTask.promise.then((pdf) => {
       setPdfDoc(pdf);
       renderPage(1);
     });
@@ -67,21 +147,84 @@ const PDFViewer = ({ pdfData, elements }) => {
     if (pdfDoc) {
       renderPage(pageNum);
     }
-  }, [pageNum, pdfDoc, elements]);  // Added elements to dependency array
+  }, [pageNum, pdfDoc, elements]);
+
+  const handleCanvasClick = (e) => {
+    // Handle click to add text element
+    const canvas = canvasRef.current;
+    const canvasRect = canvas.getBoundingClientRect();
+    const x = e.clientX - canvasRect.left;
+    const y = e.clientY - canvasRect.top;
+
+    // Check if the "Add Text" button was clicked
+    if (addingText) {
+      addTextElement({
+        type: ElementTypes.TEXT,
+        content: 'New Text',
+        position: { x, y },
+      });
+    }
+  };
+
+  const addTextElement = (newTextElement) => {
+    // Create a copy of the existing elements array and add the new text element to it
+    const updatedElements = [...elements, newTextElement];
+
+    // Update the state with the new elements array
+    setElements(updatedElements);
+  };
+
+  const handleElementSelect = (index) => {
+    // Handle selecting an element for resizing and moving
+    setSelectedElement(index);
+  };
 
   return (
-    <div>
-      <canvas ref={canvasRef} />
+    <DndProvider backend={HTML5Backend}>
       <div>
-        <button onClick={() => setPageNum(prev => Math.max(prev - 1, 1))} disabled={pageNum <= 1}>
-          Previous
-        </button>
-        <span>Page: {pageNum} / {pdfDoc?.numPages}</span>
-        <button onClick={() => setPageNum(prev => Math.min(prev + 1, pdfDoc?.numPages))} disabled={pageNum >= pdfDoc?.numPages}>
-          Next
-        </button>
+        <canvas
+          ref={canvasRef}
+          onClick={handleCanvasClick}
+          style={{ cursor: addingText ? 'crosshair' : 'auto' }}
+        />
+        <div>
+          <button
+            onClick={() => setPageNum((prev) => Math.max(prev - 1, 1))}
+            disabled={pageNum <= 1}
+          >
+            Previous
+          </button>
+          <span>
+            Page: {pageNum} / {pdfDoc?.numPages}
+          </span>
+          <button
+            onClick={() =>
+              setPageNum((prev) => Math.min(prev + 1, pdfDoc?.numPages))
+            }
+            disabled={pageNum >= pdfDoc?.numPages}
+          >
+            Next
+          </button>
+          <button onClick={() => setAddingText(!addingText)}>
+            {addingText ? 'Cancel Adding Text' : 'Add Text'}
+          </button>
+        </div>
+        <div>
+          {/* Render draggable text elements */}
+          {elements && elements?.map((element, index) => (
+            <DraggableText
+              key={index}
+              text={element.content}
+              position={element.position}
+              index={index}
+              updateElementPosition={handleElementSelect}
+              onSelect={() => handleElementSelect(index)} // Select element on click
+              isSelected={selectedElement === index}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
