7c83478456e9c39d1b05e23c4758820479db60a7
Lord Ainez <keswaspha@gmil.com>
2023-11-17 12:17:06 +0200

    notifications_masterclass(spha)

--- frontend/src/layouts/dashboard/header/NotificationsPopover.js
+++ frontend/src/layouts/dashboard/header/NotificationsPopover.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Popover from '@mui/material/Popover';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import moment from 'moment';
 import { getAllNotifications } from '../../../api/notifications';
 import { useAuthContext } from '../../../contexts/auth';
 
@@ -18,16 +24,17 @@ const NotificationsComponent = () => {
         const fetchedNotifications = await getAllNotifications();
         console.log('Fetched Notifications:', fetchedNotifications);
         setNotifications(fetchedNotifications);
-        setUnreadCount(
-          fetchedNotifications.filter(
-            (notification) => notification.status === 'unread'
-          ).length
+
+        const unreadNotifications = fetchedNotifications.filter(
+          (notification) => notification.status === 'unread'
         );
+        console.log('Unread Notifications:', unreadNotifications);
+        setUnreadCount(unreadNotifications.length);
       } catch (error) {
         console.error('Error fetching notifications:', error);
       }
     };
-    
+
     fetchNotifications();
   }, []);
 
@@ -42,20 +49,38 @@ const NotificationsComponent = () => {
   const open = Boolean(anchorEl);
 
   const formatTimeAgo = (timestamp) => {
-    const createdAt = new Date(timestamp);
-    const now = new Date();
+    const now = moment();
+    const createdAt = moment(timestamp);
+    const duration = moment.duration(now.diff(createdAt));
 
-    const timeDifference = now - createdAt;
-    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
+    if (duration.asSeconds() < 60) {
+      return 'Just now';
+    }
 
-    return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago`;
+    if (duration.asMinutes() < 60) {
+      return `${Math.floor(duration.asMinutes())} minute${Math.floor(
+        duration.asMinutes()
+      ) > 1 ? 's' : ''} ago`;
+    }
+
+    if (duration.asHours() < 24) {
+      return `${Math.floor(duration.asHours())} hour${Math.floor(
+        duration.asHours()
+      ) > 1 ? 's' : ''} ago`;
+    }
+
+    return createdAt.fromNow();
   };
 
   return (
     <div className="notifications-container">
-      <IconButton className="bell-icon" onClick={handleClick}>
-        <Badge badgeContent={unreadCount} color="error">
-          <NotificationsIcon />
+      <IconButton className="custom-bell-icon" onClick={handleClick}>
+        <Badge
+          badgeContent={unreadCount > 0 ? unreadCount : null}
+          color="error"
+          className="custom-badge"
+        >
+          <NotificationsIcon className="custom-notifications-icon" />
         </Badge>
       </IconButton>
 
@@ -71,20 +96,59 @@ const NotificationsComponent = () => {
           vertical: 'top',
           horizontal: 'right',
         }}
+        style={{ width: '200%', maxWidth: '10000%', padding: '40px' }}
       >
-        {notifications.length > 0 ? (
-          <ul className="notification-list">
-            {notifications.map((notification) => (
-              <li key={notification.id} className="notification-item">
-                <strong>{notification.title}</strong>
-                <p>{notification.message}</p>
-                <small>{formatTimeAgo(notification.created_at)}</small>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No notifications to display.</p>
-        )}
+        <Typography variant="h6" style={{ padding: '10px' }}>
+          Notifications
+        </Typography>
+
+        <List>
+          {notifications.length > 0 ? (
+            notifications.map((notification) => (
+              <ListItem
+                key={notification.id}
+                className={`custom-list-item ${
+                  notification.status === 'unread' ? 'unread-notification' : ''
+                }`}
+                style={{
+                  border: '1px solid #ddd', // Adjust the color as needed
+                  borderRadius: '8px', // Adjust the radius as needed
+                  margin: '8px 0', // Add some margin for separation
+                  padding: '8px', // Add padding for content spacing
+                  transition: 'background-color 0.3s', // Add smooth transition
+                }}
+                onMouseEnter={(e) => {
+                  // Change background color on hover
+                  if (notification.status === 'unread') {
+                    // Use a different color for unread notifications on hover
+                    e.currentTarget.style.backgroundColor = '#e0f7fa'; // Adjust the color as needed
+                  } else {
+                    e.currentTarget.style.backgroundColor = '#f5f5f5'; // Adjust the color as needed
+                  }
+                }}
+                onMouseLeave={(e) => {
+                  // Reset background color on leave
+                  e.currentTarget.style.backgroundColor = 'transparent';
+                }}
+              >
+                <ListItemAvatar>
+                  <Avatar alt="User Avatar" src={notification.userAvatar} />
+                </ListItemAvatar>
+                <Typography variant="body1">
+                  <strong>{notification.title}</strong>
+                  <br />
+                  {notification.message}
+                  <br />
+                  <small>{formatTimeAgo(notification.created_at)}</small>
+                </Typography>
+              </ListItem>
+            ))
+          ) : (
+            <Typography className="custom-no-notifications">
+              No notifications to display.
+            </Typography>
+          )}
+        </List>
       </Popover>
     </div>
   );
